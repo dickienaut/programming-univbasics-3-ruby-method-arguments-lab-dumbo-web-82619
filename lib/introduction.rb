e0d1c7c58821c3027d0d6426f0d6ae8c74ff8c09
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-02 13:34:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language='ruby')
-  puts "Hi, my name is #{name}, and I know #{language}."
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
